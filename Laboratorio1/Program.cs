965ab77bda3f7a006ee010e001d6229d861dd1a7
luzyaranga <lunazul1278@gmail.com>
2024-05-05 16:18:16 -0500

    codigo lab1

---
+++ Laboratorio1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola Clase de Estructura de Datos!");
+
+// Sumar los n primeros números
+var n = 2;
+var suma = 0;
+for ( var i = 1; i <= n; i++ )
+{
+    suma += i;
+}
+Console.WriteLine($"La suma de los {n} números es: {suma}");
