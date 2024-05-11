ac63541ce9ef31aaa4a0f2e416bda506c2eec704
luzyaranga <lunazul1278@gmail.com>
2024-05-11 01:24:51 -0500

    Código laboratorio3-4

---
+++ Laboratorio3-4/ejercicio6/Laboratorio3/ejercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Ejercicio2
+    {
+        public static void Main()
+        {
+            Console.Write("Ingresar un número entero positivo: ");
+            int n = Convert.ToInt32(Console.Read());
+
+            if (n <= 0)
+            {
+                Console.WriteLine("El número ingresado debe ser positivo.");
+            }
+            else
+            {
+                int suma = SumaNumeros(1, n);
+                Console.WriteLine("La suma de los números del 1 al " + n + " es : " + suma);
+            }
+        }
+        public static int SumaNumeros(int inicio, int n)
+        {
+            if (inicio <= n)
+            {
+                return inicio + SumaNumeros(inicio + 1, n);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
---
+++ Laboratorio3-4/ejercicio6/Laboratorio3/ejercicio3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Ingresa un número entero positivo para el tamaño de la pirámide: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (n <= 0)
+        {
+            Console.WriteLine("El número ingresado debe ser positivo.");
+        }
+        else
+        {
+            PrintPiramide(1, n);
+        }
+    }
+        public static void PrintPiramide(int numeroFilas, int n)
+        {
+            if (numeroFilas <= n)
+            {
+                PrintFilas(1, numeroFilas);
+                Console.WriteLine();
+                PrintPiramide(numeroFilas + 1, n);
+            }
+        }
+        public static void PrintFilas(int start, int end)
+        {
+            if(start <= end)
+            {
+                Console.Write(start + " ");
+                PrintFilas(start + 1, end);
+            }
+        }
+}
